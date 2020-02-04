4444bb666d6ea9fbc2a98583f0412a56e7834879
Gomes <rafael.souza@coopertec.com.br>
2020-02-04 17:36:58 -0300

    conexão api com database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController
     public function main()
     {
         $ch = curl_init();
-        $url = 'http://localhost:4040/categorias';
+        $url = 'http://localhost:4000/categorias';
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $resp = json_decode(curl_exec($ch), true);
